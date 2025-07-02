e30683473efd7f4e87d56861f35c8bbb51aab6f1
arminas-bartkus <arminas.bartkus@gmail.com>
2025-07-02 23:00:11 +0100

    Add addition to table functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-bookArr = [];
+let bookArr = [];
+let counter = 1;  
 
 revealBookFormButton = document.querySelector(".addBook");
 addBookForm = document.querySelector(".addBookForm");
@@ -13,37 +14,96 @@ bookAuthorInput = document.querySelector("#bookAuthor");
 numberOfPagesInput = document.querySelector("#noOfPages");
 isBookReadInput = document.querySelector("#userRead");
 
+// table definitions for table manipulation
+
+libraryTable = document.querySelector("table");
+
 
 
 revealBookFormButton.addEventListener("click", function () {
+    
+    // Make function for this
+
+    bookTitleInput.value = "";
+    bookAuthorInput.value = "";
+    numberOfPagesInput.value = "";
+    isBookReadInput.checked;
+    
     addBookForm.showModal();
 });
 
 
-function Book(title, author, numberOfPages, isRead) {
+function Book(title, author, numberOfPages, isRead, uniqueId) {
     
     this.title  = title;
     this.author = author;
     this.numberOfPages = numberOfPages;
     this.isRead = isRead;
+    this.uniqueId = uniqueId;
 }
 
 submitFormButton.addEventListener("click", function() {
-    addBookToLibrary()
+    
+    let uniqueId = createUniqueId()
+    addBookToLibrary(uniqueId)
 });
 
-function addBookToLibrary() {
-        
+function addBookToLibrary(uniqueId) {
+    let i = 0;
     let bookIsRead = false;
     let fetchedBookTitle = bookTitleInput.value;
     let fetchedBookAuthor = bookAuthorInput.value;
     let fetchedPageCount = numberOfPagesInput.value;
     
     if (isBookReadInput.checked) {
-        bookIsRead = true;
+        bookIsRead = "Yes"
     }
-    else {}
+    else {bookIsRead = "No"}
 
-    bookArr.push(new Book(fetchedBookTitle, fetchedBookAuthor, fetchedPageCount, bookIsRead));
+    bookArr.push(new Book(uniqueId, fetchedBookTitle, fetchedBookAuthor, fetchedPageCount, bookIsRead));
 
+
+    let newRow = libraryTable.insertRow(counter);
+    newRow.classList.add("row" + counter.toString());
+    counter++;
+    
+    let lastObjToArr = Object.values(bookArr[bookArr.length-1]);
+   
+
+    
+    lastObjToArr.forEach((item) => {
+        
+        if (bookIsRead) {
+            bookIsRead = "Yes"
+        }
+        else {
+            bookIsRead = "No"
+        }
+        
+
+        newCell = newRow.insertCell(i);
+        newCell.innerHTML = item;
+        i++
+    });
+    
+    // for (i = 0; i <= 4; i++) {
+    //     newCell = newRow.insertCell(i);
+    //     newCell.innerHTML = fetchedBookTitle;
+    // }
+
+    // Create empty data Cells
+    // create unique row with unique class? make like 5 td in it, and then iteratively add the items one by 1
+    // Loop to add to table
+
+    // with unique classes, it will be easy to remove an entire row for example,
+
+    // insertrow() at 0 (always at top) (have a separate counter that counts the number of rows used to give them unique class names)
+    // will make easier to delete
+    // insert cell for items in object, i.e. create cell with cell index = item.count-1 and insert item property.value into said cell.
 }
+
+function createUniqueId() {
+    let uniqueId = self.crypto.randomUUID();
+    return uniqueId;
+}
+
